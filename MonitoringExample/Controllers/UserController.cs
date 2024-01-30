846887b6628f9722f5286072916e7afdd07cf638
omid ahmadpour <ahmadpooromid@gmail.com>
2024-01-30 17:19:32 +0100

    Add project files.

---
+++ MonitoringExample/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace MonitoringExample.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly Counter<int> matchesCreatedCounter;
+
+    public UserController(Meter meter)
+    {
+        this.matchesCreatedCounter = meter.CreateCounter<int>("matches_created");
+    }
+
+    [HttpGet("hello")]
+    public IActionResult Hello()
+    {
+        return Ok("Hello, ASP.NET Core API!");
+    }
+
+    [HttpPost("createMatch")]
+    public IActionResult CreateMatch()
+    {
+        var matchId = Guid.NewGuid();
+
+        matchesCreatedCounter.Add(1);
+
+        return Ok($"Match created with ID: {matchId}");
+    }
+}
---
+++ MonitoringExample/Program.cs
@@ -0,0 +1,35 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+using System.Diagnostics.Metrics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<Meter>((serviceProvider) =>
+{
+    var meter = new Meter("MatchMaker.Api");
+    return meter;
+});
+
+builder.Services.AddOpenTelemetry().UseAzureMonitor();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
